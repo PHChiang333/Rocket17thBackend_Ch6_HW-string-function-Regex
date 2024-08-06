4aa571aba150ee0d6d47abd616bb6b3080d20111
PHChiang333 <frank79530@gmail.com>
2024-08-07 00:21:00 +0800

    feat:1st commit

---
+++ Rocket17thBackend_Ch6_HW/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket17thBackend_Ch6_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
